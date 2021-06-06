af002118d4d5e93420b1e6b40e64991cd5101bf1
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-06-06 20:59:51 +0200

    Add finished FormationUtilities::getFormattedScore
    - Add finished FormationUtilities::getFormattedScore
    - Add FormationUtilities::getFormattedTime

--- src/main/java/de/timmi6790/mineplex/stats/common/utilities/FormationUtilities.java
+++ src/main/java/de/timmi6790/mineplex/stats/common/utilities/FormationUtilities.java
@@ -9,16 +9,28 @@ import java.text.NumberFormat;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 @UtilityClass
 public class FormationUtilities {
+    // TODO: Find a better solution for this. It should be handled by the api
+    private static final List<String> STATS_TIME = Arrays.asList("IngameTime", "HubTime", "TimePlaying");
+
     private static final DecimalFormat FORMAT_NUMBER = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+    private static final DecimalFormat FORMAT_DECIMAL_POINT = new DecimalFormat(".##");
 
     static {
         final DecimalFormatSymbols numberSymbol = FORMAT_NUMBER.getDecimalFormatSymbols();
         numberSymbol.setGroupingSeparator(',');
         FORMAT_NUMBER.setDecimalFormatSymbols(numberSymbol);
+
+        final DecimalFormatSymbols dateSymbol = FORMAT_DECIMAL_POINT.getDecimalFormatSymbols();
+        dateSymbol.setDecimalSeparator('.');
+        FORMAT_DECIMAL_POINT.setDecimalFormatSymbols(dateSymbol);
+
     }
 
     public String getFormattedTime(final ZonedDateTime zonedDateTime) {
@@ -34,10 +46,50 @@ public class FormationUtilities {
         return (stat.isAchievement() ? "Achievement" : "") + stat.getCleanName();
     }
 
+    protected String getFormattedTime(long time) {
+        final long days = TimeUnit.SECONDS.toDays(time);
+        time -= TimeUnit.DAYS.toSeconds(days);
+
+        final long hours = TimeUnit.SECONDS.toHours(time);
+        time -= TimeUnit.HOURS.toSeconds(hours);
+
+        if (days != 0) {
+            if (hours == 0) {
+                return days + (days > 1 ? " days" : " day");
+            }
+
+            return days + FORMAT_DECIMAL_POINT.format(hours / 24D) + " days";
+        }
+
+        final long minutes = TimeUnit.SECONDS.toMinutes(time);
+        time -= TimeUnit.MINUTES.toSeconds(minutes);
+        if (hours != 0) {
+            if (minutes == 0) {
+                return hours + (hours > 1 ? " hours" : " hour");
+            }
+
+            return hours + FORMAT_DECIMAL_POINT.format(minutes / 60D) + " hours";
+        }
+
+        final long seconds = TimeUnit.SECONDS.toSeconds(time);
+        if (minutes != 0) {
+            if (seconds == 0) {
+                return minutes + (minutes > 1 ? " minutes" : " minute");
+            }
+
+            return minutes + FORMAT_DECIMAL_POINT.format(seconds / 60D) + " minutes";
+        }
+
+        return seconds + (seconds > 1 ? " seconds" : " second");
+    }
+
     public String getFormattedScore(final Stat stat, final long score) {
-        // TODO: Handle stat specific things for time
         if (score == -1) {
             return "Unknown";
+        }
+
+        if (STATS_TIME.contains(stat.getStatName())) {
+            return getFormattedTime(score);
         }
 
         return FormationUtilities.getFormattedNumber(score);
