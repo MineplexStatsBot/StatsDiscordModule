d39a225f9233f0df5a43858814e98b66c303eea3
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-06-07 10:18:07 +0200

    Add FilterReasonSetting empty set option
    - Add FilterReasonSetting empty set option

--- src/main/java/de/timmi6790/mineplex/stats/common/settings/FilterReasonSetting.java
+++ src/main/java/de/timmi6790/mineplex/stats/common/settings/FilterReasonSetting.java
@@ -12,6 +12,7 @@ import java.util.*;
 @Log4j2
 public class FilterReasonSetting extends AbstractSetting<Set<Reason>> {
     private static final String REPOSITORY_DIVIDER = "/";
+    private static final String EMPTY_SET_ARGUMENT = "None";
     private static final Set<Reason> DEFAULT_REASONS = EnumSet.of(Reason.GLITCHED, Reason.GIVEN);
 
     public static Set<Reason> getDefaultReasons() {
@@ -23,6 +24,7 @@ public class FilterReasonSetting extends AbstractSetting<Set<Reason>> {
                 "FilterReasons",
                 "Filter reasons that should be excluded." +
                         "\nPossible values: " + String.join(", ", EnumUtilities.getPrettyNames(Reason.values())) +
+                        "\nUse " + MarkdownUtil.monospace(EMPTY_SET_ARGUMENT) + " to have no filters at all" +
                         "\nDivide new input with " + MarkdownUtil.bold(","),
                 DEFAULT_REASONS
         );
@@ -30,6 +32,10 @@ public class FilterReasonSetting extends AbstractSetting<Set<Reason>> {
 
     @Override
     public String toDatabaseValue(final Set<Reason> value) {
+        if (value.isEmpty()) {
+            return EMPTY_SET_ARGUMENT;
+        }
+
         // There is no reason to try to save disk space here for the limited usage
         // We can't use , or ; as the divider, this is currently not working with the main framework. I should really fix this
         final StringJoiner joinedReasons = new StringJoiner(REPOSITORY_DIVIDER);
@@ -43,6 +49,10 @@ public class FilterReasonSetting extends AbstractSetting<Set<Reason>> {
     @Override
     public Set<Reason> fromDatabaseValue(final String value) {
         final Set<Reason> found = EnumSet.noneOf(Reason.class);
+        if (EMPTY_SET_ARGUMENT.equalsIgnoreCase(value)) {
+            return found;
+        }
+
         final String[] parts = value.split(REPOSITORY_DIVIDER);
         for (final String part : parts) {
             try {
@@ -59,6 +69,10 @@ public class FilterReasonSetting extends AbstractSetting<Set<Reason>> {
     @Override
     protected Optional<Set<Reason>> parseNewValue(final CommandParameters commandParameters, final String userInput) {
         final Set<Reason> found = EnumSet.noneOf(Reason.class);
+        if (EMPTY_SET_ARGUMENT.equalsIgnoreCase(userInput)) {
+            return Optional.of(found);
+        }
+
         final String[] parts = userInput.split(",");
         for (final String part : parts) {
             EnumUtilities.getIgnoreCase(part.trim(), Reason.values()).ifPresent(found::add);
